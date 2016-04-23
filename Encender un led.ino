1319ffb546b51f020d77161929ca34edb8e36c71
juanlu92 <juanlu92@users.noreply.github.com>
2016-04-23 10:14:20 +0200

    Update Encender un led.ino

--- Encender un led.ino
+++ Encender un led.ino
@@ -2,7 +2,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
 }
-
+//Este codigo me permite que el led se encienda y se apague cada 3 segundos
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)

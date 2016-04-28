21f5d5889dac78e81030b864e4860757ba27933f
Index26694 <sergio_coloso@hotmail.com>
2016-04-28 19:34:52 +0200

    Added files via upload

---
+++ Clases provisionales/casa.php
@@ -0,0 +1,17 @@
+<?php
+class casa {
+    //-------EDITAR CASA-------
+  public $editarcasa;
+  public $editarpropiedadcasa;
+  public $editarcomponentescasa;
+  public $numerohabitacionescasa;
+  public $clavecasa;
+  //------EDITAR DIRECTION CASA-------
+  public $reemplazodirectioncasa;
+  public $clavedirectioncasa;
+  //-----ALTA CASA-------
+  public $nombrealtacasacasa;
+  public $numerohabitacionescasa;
+  public $direccionaltacasa;
+  //-------BAJA CASA---------
+  public $clavebajacasa;
---
+++ Clases provisionales/habitaciones.php
@@ -0,0 +1,14 @@
+<?php
+class habitaciones {
+    //-------EDITAR HABITACIONES-------
+  public $editarcomponentes;
+  public $editarpropiedadcomponentes;
+  public $editarclavecomponentes;
+  public $editartipocomponentes;
+  //-----ALTA componentes-------
+  public $nombrealtacomponentes;
+  public $tipocomponentes;
+  public $nombrepropiedadcomponentes;
+  public $numerocomponentes;
+  //-------BAJA componentes---------
+  public $clavebajacomponentes;
